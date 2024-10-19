bb0040b71f6a236aa13d774621412c87eb84f3af
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-20 00:47:39 +0530

    sum of elements of an array through recursion

--- recursiveBasics.js
+++ recursiveBasics.js
@@ -1,10 +1,22 @@
 //// exponent
 function exponents(b, e) {
-    if(e === 0){
+    if (e === 0) {
         return 1;
     }
 
     return b * exponents(b, e - 1);
 }
 
-console.log(exponents(3, 3))
+// console.log(exponents(3, 3))
+
+
+//// Sum of the element
+
+function sumArray(arr) {
+    if (arr.length === 0) {
+        return 0
+    }
+    return arr.slice(-1)[0] + sumArray(arr.slice(0, arr.length - 1))
+}
+
+console.log(sumArray([1, 2, 3, 4, 5]))
---
+++ test.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4,5,6]
+
+let newArr = arr.slice(-1)
+
+console.log(newArr)
