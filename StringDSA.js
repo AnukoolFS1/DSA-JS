d73647ca8980641221c18e9bf4334b161d82a80e
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-20 00:33:07 +0530

    done with a vowel count dsa

--- StringDSA.js
+++ StringDSA.js
@@ -4,7 +4,7 @@ let string = "developer"
 function reverseString(str) {
     let reversed = ''
 
-    for(let i = str.length - 1; i>=0; i--){
+    for (let i = str.length - 1; i >= 0; i--) {
         reversed += str[i]
     }
 
@@ -14,3 +14,23 @@ function reverseString(str) {
 console.log(reverseString(string))
 
 ////// count vowel /////
+function countVowel(str) {
+    count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i].toLowerCase()) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                count++;
+                break;
+            default:
+        }
+    }
+
+    return count
+}
+
+console.log(countVowel(string))
