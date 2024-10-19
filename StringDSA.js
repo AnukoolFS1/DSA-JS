a5bf33cd3f30c6511d6d69e70cc21780d8f73bcd
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-20 00:29:07 +0530

    added a string dsa

---
+++ StringDSA.js
@@ -0,0 +1,16 @@
+///// reverse string //////
+let string = "developer"
+
+function reverseString(str) {
+    let reversed = ''
+
+    for(let i = str.length - 1; i>=0; i--){
+        reversed += str[i]
+    }
+
+    return reversed
+}
+
+console.log(reverseString(string))
+
+////// count vowel /////
