b633d2683daceb51219f25668cce3aeb73b81608
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-12-29 20:26:33 +0530

    self map function

--- BasicRandomQuestions/args.js
+++ BasicRandomQuestions/args.js
@@ -3,4 +3,17 @@ var argumentsLength = function(...args) {
 };
 
 
-console.log(argumentsLength(1,2,3,4))
+// console.log(argumentsLength(1,2,3,4))
+
+// Apply Transformation for each element of array
+var map = function(arr, fn) {
+    let newArr = []
+    for(let e = 0; e<arr.length; e++){
+        newArr.push(fn(arr[e], e))
+    }
+    return newArr
+};
+
+map([1,2,3], function(n, i){return n + 1})
+map([1,2,3], function(n, i){return n + i})
+map([1,2,3], function(n, i){return 42})
