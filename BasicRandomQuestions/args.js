623a8cd68a5e3416b139fd71db6ee21b9af9aee0
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-12-29 20:32:08 +0530

    filter

--- BasicRandomQuestions/args.js
+++ BasicRandomQuestions/args.js
@@ -17,3 +17,17 @@ var map = function(arr, fn) {
 map([1,2,3], function(n, i){return n + 1})
 map([1,2,3], function(n, i){return n + i})
 map([1,2,3], function(n, i){return 42})
+
+
+//Filter
+var filter = function(arr, fn) {
+    const newArr = [];
+    for(let i = 0; i<arr.length;i++){
+        if(fn(arr[i])) newArr.push(arr[i])
+    }
+
+    return newArr
+};
+
+filter([0,10,20,30], fn = function greaterThan10(n) { return n > 10; })
+filter([-1,-2,0,2,1], fn = function greaterThan10(n) { return n + 1; })
